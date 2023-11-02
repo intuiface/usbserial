a0020ed473fff6cb0918dfef107a40df64c05170
Nicolas Daures <daures@intuiface.com>
2023-11-02 12:25:46 +0100

    PN-3212: fix pending intent parameters

--- src/android/UsbSerial.java
+++ src/android/UsbSerial.java
@@ -147,7 +147,7 @@ public class UsbSerial extends CordovaPlugin {
 					}
 
 					// create the intent that will be used to get the permission
-					PendingIntent pendingIntent = PendingIntent.getBroadcast(cordova.getActivity(), 0, new Intent(UsbBroadcastReceiver.USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
+					PendingIntent pendingIntent = PendingIntent.getBroadcast(cordova.getActivity(), 0, new Intent(UsbBroadcastReceiver.USB_PERMISSION), PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
 					// and a filter on the permission we ask
 					IntentFilter filter = new IntentFilter();
 					filter.addAction(UsbBroadcastReceiver.USB_PERMISSION);
